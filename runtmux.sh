157582f313c43162dfc18590e533e30ee7e8ac90
rentorx <ingrenetorres@gmail.com>
2019-06-04 00:40:19 -0500

    improving environment

---
+++ runtmux.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+session=$1
+path=$2
+
+cd $path
+
+workon $session
+# create a new tmux session
+tmux has-session -t $session
+
+if [ $? -eq 0 ]; then
+  tmux ls
+  tmux attach-session -t $session
+else
+  # set up tmux
+  tmux start-server
+  tmux new-session -d -s $session
+  # Pane 1 and split
+  tmux selectp -t 1
+  tmux splitw -h -p 35
+
+  # Pane 2 and split
+  tmux selectp -t 2
+  tmux splitw -v -p 75
+
+  tmux send-keys -t $session:1.0 "workon $session" C-m
+  tmux send-keys -t $session:1.1 "workon $session" C-m
+  tmux send-keys -t $session:1.2 "workon $session" C-m
+  tmux attach-session -t $session
+
+fi
